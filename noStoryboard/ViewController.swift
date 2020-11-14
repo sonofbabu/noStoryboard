9282822544cbf617f6d6db52a99898624a0a5d76
leyo babu <leyo@oustlabs.com>
2020-11-15 00:18:28 +0530

    Initial Commit

---
+++ noStoryboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  noStoryboard
+//
+//  Created by leyo babu on 15/11/20.
+//  Copyright © 2020 leyo babu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
